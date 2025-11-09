d92ff126b18494669ac3ef31bd368cde95199bf5
moxiuu <moxiuux@proton.me>
2025-11-09 17:26:01 +0100

    small note Signed-off-by: moxiuu <moxiuux@proton.me>

--- init_a53x.sh
+++ init_a53x.sh
@@ -10,3 +10,4 @@ axion_dir="$HOME/roms/axion/"
 echo "moving manifests..."
 mv $HOME/my_manifests/s5e8825/s5e8825.xml ~/roms/axion/.repo/manifests/
 mv $HOME/my_manifests/s5e8825/manifest.xml ~/roms/axion/.repo/
+# that's all for now i guess
